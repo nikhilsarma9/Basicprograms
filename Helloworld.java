72d253d80977c16d98e03ec6c0225452762f3cc3
sainikhil <109243188+nikhilsarma9@users.noreply.github.com>
2022-11-18 23:04:09 -0800

    update by testing

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,18 @@
 
+public class test
+{
+	int add()
+	{
+	int a=10,b=23,c;
+	return c=a+b;
+	}
+}
 public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("No im starting now");
+		test adding=new test();
+		System.out.println(adding.add());
 
 	}
 
